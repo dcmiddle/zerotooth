e7756b8187bc2494219c6974172735f129993675
Dan Middleton <dan.middleton@intel.com>
2018-07-07 10:39:02 -0500

    Fixup: add main.cpp
    main.cpp acts as a test harness on the circuit now.
    
    Signed-off-by: Dan Middleton <dan.middleton@intel.com>

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <intkey-circuit-gl1.hpp>
+
+using std::cout;
+using std::endl;
+using namespace libsnark;
+
+//Note: libsnark::default_r1cs_ppzksnark_pp is libff::default_ec_pp
+typedef libsnark::default_r1cs_ppzksnark_pp ppzksnark_ppT;
+typedef libff::Fr<libff::default_ec_pp> Fp;
+
+int main() {
+    ppzksnark_ppT::init_public_params();
+
+    IntkeyCircuit<Fp, ppzksnark_ppT> circuit;
+    circuit.generate();
+
+    uint32_t my_secret_value = 33;
+    r1cs_ppzksnark_proof<ppzksnark_ppT> proof;
+    proof = circuit.prove(my_secret_value);
+
+    if(circuit.verify(proof))
+        cout << "Valid Proof" << endl;
+    else
+        cout << "DANGER Invalid Proof" << endl;
+
+    return 0;
+}
